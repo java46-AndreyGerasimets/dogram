1e6657bf9edb257d71da07e3311b8efe361c7817
ferretvl <andrey20k@gmail.com>
2022-01-02 15:40:33 +0200

    HW07 Additional task

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ console.log("Hello world!")
 let detailsImage = document.querySelector(".details-image");
 let detailsTitle = document.querySelector(".details-title");
 let anchors = document.querySelectorAll(".thumbnails-anchor");
+
+setDetails(anchors[0]);
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function(event) {
         event.preventDefault();
@@ -10,5 +12,6 @@ for(let i = 0; i < anchors.length; i++) {
 }
 function setDetails(anchor) {
     detailsImage.setAttribute("src", anchor.getAttribute("href"));
-    detailsTitle.textContent = anchor.getAttribute("data-details-title");
+    let thumbnailsTitle = anchor.querySelector(".thumbnails-title").innerHTML;
+    detailsTitle.textContent = thumbnailsTitle + " : " + anchor.getAttribute("data-details-title");
 }

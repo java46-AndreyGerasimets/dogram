8354736f86b6b6efc8dd9dd7cb2a1b271b45a40b
ferretvl <andrey20k@gmail.com>
2022-01-09 10:14:47 +0200

    Adds sounds. Removes bug with second click on same thumbnail. Removes action for second click.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,44 @@
 let detailsImage = document.querySelector(".details-image");
 let detailsTitle = document.querySelector(".details-title");
 let mainContentEl = document.querySelector(".main-content");
+let detailsSound = document.querySelector(".details-sound");
 
 let selectedItem;
-let anchors = 
-document.querySelectorAll(".thumbnails-anchor");
+let anchors = document.querySelectorAll(".thumbnails-anchor");
+
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function(event) {
         event.preventDefault();
-        showDetails();
-        setDetails(anchors[i]);
+        if (selectedItem != anchors[i].parentElement) {
+            showDetails();
+            setDetails(anchors[i]);
+        }
     })
 }
+
 function setDetails(anchor) {
-    console.log("anchor element  was pressed", anchor);
     let hrefValue = anchor.getAttribute("href");
-    detailsImage.setAttribute("src", hrefValue );
+    detailsImage.setAttribute("src", hrefValue);
     anchor.parentElement.classList.add("selected");
     if (selectedItem) {
-        selectedItem.classList.remove("selected")
+            selectedItem.classList.remove("selected")
     }
     selectedItem = anchor.parentElement;
     let thumbnailsTitleSelector = `[href="${hrefValue}"] .thumbnails-title`;
     let thumbnailsTitleEl = document.querySelector(thumbnailsTitleSelector);
     detailsTitle.textContent = `${thumbnailsTitleEl.textContent}: ${anchor.getAttribute('data-details-title')}` ;
-   
+    let sound = document.querySelector(`[href="${hrefValue}"] .thumbnails-sound`).getAttribute("src");
+    detailsSound.setAttribute("src", sound);
+    detailsSound.muted = false;
+    setTimeout((function() {detailsSound.muted = true;}), 10000);
 }
+
 function showDetails() {
     mainContentEl.classList.remove('hidden');
     detailsImage.parentElement.classList.add('is-tiny');
     setTimeout(removeIsTiny);
-    
-
-
 }
+
 function removeIsTiny() {
     detailsImage.parentElement.classList.remove('is-tiny');
 }
